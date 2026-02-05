ac68609d30e12f1b7d3efa05e0fe38ecc963677a
kangddong <rkdehddud96@naver.com>
2026-02-05 13:25:10 +0900

    chore: add initial project

---
+++ Dori-iOS/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Dori-iOS
+//
+//  Created by 강동영 on 2/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ Dori-iOS/DoriApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoriApp.swift
+//  Dori-iOS
+//
+//  Created by 강동영 on 2/5/26.
+//
+
+import SwiftUI
+
+@main
+struct DoriApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
