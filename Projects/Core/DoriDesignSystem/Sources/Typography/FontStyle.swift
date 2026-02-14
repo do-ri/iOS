d1cca1a67e2a4acd9b3f42cc3cc649c80c8429ad
kangddong <rkdehddud96@naver.com>
2026-02-14 20:33:32 +0900

    feat: TypoStyle -> TypoToken /TypoSemantic으로 분리
    - 현재 디자인이 Semantic으로 100% 전환되지않은 케이스가 있어 TypoToken / TypoSemantic 두가지를 병행하여 사용
    - .lineSpacing()에 Line height 적용하며 버그 발생, linespacing, lineheight 개념 숙지 필요.. !
    
    # Conflicts:
    #	Projects/Core/DoriDesignSystem/Sources/Typography/TypoToken.swift

--- Projects/Core/DoriDesignSystem/Sources/Typography/FontStyle.swift
+++ Projects/Core/DoriDesignSystem/Sources/Typography/FontStyle.swift
@@ -9,33 +9,34 @@ import SwiftUI
 
 // MARK: - Font 타입 속성을 정의하는 구조체
 public enum FontType: Equatable {
-    case system(Font.Weight)
-    case custom(String)
+  case system(Font.Weight)
+  case custom(String)
 }
 
 // MARK: - TypoStyle 혹은 직접 활용도 가능하끔 구조 작성
 @MainActor
 public struct FontStyle {
-    public let font: Font
-    public let fontSize: CGFloat
-    public let lineHeight: CGFloat
+  public let font: Font
+  public let fontSize: CGFloat
+  public let lineHeight: CGFloat
+  public let lineSpacing: CGFloat
+  
+  public init(
+    _ family: FontType = .system(.regular),
+    size: CGFloat = 16
+  ) {
+    self.fontSize = size
     
-    public init(
-        _ family: FontType = .system(.regular),
-        size: CGFloat = 16,
-        lineHeight: CGFloat = 1.5,
-    ) {
-        self.fontSize = size
-        
-        switch family {
-        case .system(let weight):
-            self.font = .system(size: size, weight: weight)
-        case .custom(let fontName):
-            // Bundle.module에서 폰트 등록 후 사용
-            FontManager.registerFontIfNeeded(fontName)
-            self.font = .custom(fontName, size: size)
-        }
-
-        self.lineHeight = size * lineHeight - size
+    switch family {
+    case .system(let weight):
+      self.font = .system(size: size, weight: weight)
+    case .custom(let fontName):
+      // Bundle.module에서 폰트 등록 후 사용
+      FontManager.registerFontIfNeeded(fontName)
+      self.font = .custom(fontName, size: size)
     }
+    // 등차수열 적용
+    self.lineHeight = 2 * size - 6
+    self.lineSpacing = lineHeight - fontSize
+  }
 }
