7a135fdfa3e88831a78670d76ae803571a155b6a
kangddong <rkdehddud96@naver.com>
2026-02-18 18:46:12 +0900

    feat: 피처 간 공유 되는 객체들 구현

---
+++ Projects/Core/DoriDesignSystem/Sources/FloatingActionButton.swift
@@ -0,0 +1,55 @@
+//
+//  FloatingActionButton.swift
+//  Dori-iOS
+//
+//  Created by 강동영 on 2/15/26.
+//
+
+import SwiftUI
+
+public struct FloatingActionButton: View {
+  let action: () -> Void
+
+  public init(action: @escaping () -> Void) {
+    self.action = action
+  }
+
+  public var body: some View {
+    Button(action: action) {
+      Image(systemName: "plus")
+        .font(.title2)
+        .fontWeight(.semibold)
+        .foregroundColor(.white)
+        .frame(
+          width: 56,
+          height: 56
+        )
+        .background(DoriColors.main.color)
+        .clipShape(Circle())
+        .shadow(
+          color: .black.opacity(0.3),
+          radius: 4,
+          x: 0,
+          y: 2
+        )
+    }
+  }
+}
+
+#Preview {
+  ZStack {
+    Color.gray.opacity(0.2)
+      .ignoresSafeArea()
+
+    VStack {
+      Spacer()
+      HStack {
+        Spacer()
+        FloatingActionButton {
+          print("FAB tapped")
+        }
+        .padding()
+      }
+    }
+  }
+}
